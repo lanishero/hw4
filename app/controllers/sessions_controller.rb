75827fdee9117620b73e03a8402f9e310651097e
lanishero <lanchen.song@kellogg.northwestern.edu>
2022-03-15 19:47:49 +0000

    asdf

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,24 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.where({ email:params["email"] })[0]
+    if @user
+      if @user.password == params["password"]
+        flash[:notice] = "You are logged in!"
+        redirect_to "/places"
+      else
+        flash[:notice] = "Wrong password!"
+      redirect_to "/sessions/new"
+      end
+    else
+      flash[:notice] = "Wrong email!"
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
   end
+
+
 end
   
